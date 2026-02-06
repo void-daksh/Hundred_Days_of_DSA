2cc60035bd6344945ef7aeaea63a56e86b82993c
void-daksh <itzdaksh17@gmail.com>
2026-02-06 21:55:50 +0530

    code

---
+++ Day_6/Q2.c
@@ -0,0 +1,40 @@
+/*Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Note that you must do this in-place without making a copy of the array.
+
+ 
+
+Example 1:
+
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+Example 2:
+
+Input: nums = [0]
+Output: [0]*/
+
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int nums[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &nums[i]);
+    }
+    int k = 0;
+    for (int i = 0; i < n; i++) {
+        if (nums[i] != 0) {
+            nums[k++] = nums[i];
+        }
+    }
+    while (k < n) {
+        nums[k++] = 0;
+    }
+    for (int i = 0; i < n; i++) {
+        printf("%d ", nums[i]);
+    }
+
+    return 0;
+}
