160ee411457b97adb54f22e2db55dd3fadbb11ff
void-daksh <itzdaksh17@gmail.com>
2026-02-08 20:50:50 +0530

    code

---
+++ Day_8/Q2.c
@@ -0,0 +1,41 @@
+/*Given an integer n, return true if it is a power of two. Otherwise, return false.
+
+An integer n is a power of two, if there exists an integer x such that n == 2x.
+
+ 
+
+Example 1:
+
+Input: n = 1
+Output: true
+Explanation: 20 = 1
+Example 2:
+
+Input: n = 16
+Output: true
+Explanation: 24 = 16
+Example 3:
+
+Input: n = 3
+Output: false*/
+
+#include <stdio.h>
+#include <stdbool.h>
+
+bool isPowerOfTwo(int n) {
+    if (n <= 0)
+        return false;
+    return (n & (n - 1)) == 0;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    if (isPowerOfTwo(n))
+        printf("True\n");
+    else
+        printf("False\n");
+
+    return 0;
+}
