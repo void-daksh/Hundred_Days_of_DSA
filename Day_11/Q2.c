f9a5a47c2e8cfe1c3cd38a74d9453b9dd438e724
void-daksh <itzdaksh17@gmail.com>
2026-02-16 21:28:49 +0530

    code

---
+++ Day_11/Q2.c
@@ -0,0 +1,47 @@
+/*Given a 2D integer array matrix, return the transpose of matrix.
+
+The transpose of a matrix is the matrix flipped over its main diagonal, switching the matrix's row and column indices
+Example 1:
+
+Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [[1,4,7],[2,5,8],[3,6,9]]
+Example 2:
+
+Input: matrix = [[1,2,3],[4,5,6]]
+Output: [[1,4],[2,5],[3,6]]*/
+
+#include <stdio.h>
+
+int main() {
+    int rows, cols;
+
+    // Input number of rows and columns
+    scanf("%d %d", &rows, &cols);
+
+    int matrix[rows][cols];
+    int transpose[cols][rows];
+
+    // Input matrix elements
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < cols; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    // Compute transpose
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < cols; j++) {
+            transpose[j][i] = matrix[i][j];
+        }
+    }
+
+    // Print transpose matrix
+    for(int i = 0; i < cols; i++) {
+        for(int j = 0; j < rows; j++) {
+            printf("%d ", transpose[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
